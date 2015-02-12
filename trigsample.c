1b2f2e344d6accd8740fd263ae4aeb630449ff06
jzu <jzu@free.fr>
2015-02-12 11:45:45 +0100

    License blurb

--- trigsample.c
+++ trigsample.c
@@ -1,3 +1,22 @@
+/****************************************************************************
+ * TrigSample
+ *
+ * Copyright (C) Jean Zundel <jzu@free.fr> 2015 
+ *
+ * Doubles a signal with another in a separate WAV file.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ *****************************************************************************/
+
 #include <unistd.h>
 #include <stdio.h>
 #include <sys/stat.h>
